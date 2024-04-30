7e6ac1211848446ebee18b68c1968e25e23dd0a0
gh.lee <ingan6951@naver.com>
2024-04-30 12:41:23 +0900

    comii

---
+++ 240430.ipynb.py
@@ -0,0 +1,24 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "base (Python 3.11.8)에 연결됨"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "base",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.11.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
