062791e7e172431719a03e9e3f8d4cabda1bef47
Nooooov <79398889+GyuhyeongLee@users.noreply.github.com>
2024-05-03 04:54:36 +0000

    add antdesign system color

--- new_app/src/App.js
+++ new_app/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { blue } from '@ant-design/colors';
 
 function App() {
   return (
     <div className='App'>
       <div className='black-nav'>
-        <h4>블로그</h4>
+        <h4 color='blue'>블로그</h4>
       </div>
     </div>
   )
 }
+
+console.log(blue); // ['#E6F4FF', '#BAE0FF', '#91CAFF', '#69B1FF', '#4096FF', '#1677FF', '#0958D9', '#003EB3', '#002C8C', '#001D66']
+console.log(blue.primary); // '#1677FF'
 
 export default App;
